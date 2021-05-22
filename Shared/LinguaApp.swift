454d4efcce52d80178941c3fa0dd2be3106a4a1f
Richard Piazza <richard@richardpiazza.com>
2021-05-22 16:48:17 -0500

    Initial Project Setup & Assets

---
+++ Shared/LinguaApp.swift
@@ -0,0 +1,20 @@
+//
+//  LinguaApp.swift
+//  Shared
+//
+//  Created by Richard Piazza on 5/22/21.
+//
+
+import SwiftUI
+
+@main
+struct LinguaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
